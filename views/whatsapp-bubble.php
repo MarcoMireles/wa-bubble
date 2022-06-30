4ca5ca5357cddaec675dee9aa11b22e8609b59b3
Marco Mireles <marcomirelesvilla@gmail.com>
2022-06-30 10:38:16 -0500

    Estructura Whatsapp Bubble popup

--- views/whatsapp-bubble.php
+++ views/whatsapp-bubble.php
@@ -1,5 +1,20 @@
-<div id="wa-bubble" class="wa-bubble whatsapp-container">
-  <div class="wa-bubble whatsapp-container-icon <?php echo (isset(WA_Bubble_Settings::$options_style['wa_bubble_bubble_side'])) ? esc_attr(WA_Bubble_Settings::$options_style['wa_bubble_bubble_side']) : 'right';  ?>">
+<div id="wa-bubble" class="wa-bubble whatsapp-container <?php echo (isset(WA_Bubble_Settings::$options_style['wa_bubble_bubble_side'])) ? esc_attr(WA_Bubble_Settings::$options_style['wa_bubble_bubble_side']) : 'right';  ?>">
+  <div class="wa-bubble whatsapp-popup-container">
+    <div class="wa-bubble whatsapp-popup-box">
+      <div class="wa-bubble whatsapp-popup-header">
+        <p class="popup-header">Whatsapp</p>
+        <p class="close-popup">X</p>
+      </div>
+      <div class="wa-bubble whatsapp-popup-content">
+        <p class="popup-message">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <textarea id="" cols="30" rows="10"></textarea>
+      </div>
+      <div class="wa-bubble whatsapp-popup-footer">
+        <a href="#" class="popup-submit-button" target="_blank">Submit</a>
+      </div>
+    </div>
+  </div>
+  <div class="wa-bubble whatsapp-container-icon ">
     <div class="wa-bubble whatsapp-icon ">
       <img class="<?php echo (isset(WA_Bubble_Settings::$options_style['wa_bubble_bubble_size'])) ? esc_attr(WA_Bubble_Settings::$options_style['wa_bubble_bubble_size']) : 'medium';  ?>" src="<?php echo WA_BUBBLE_URL . 'assets/img/whatsapp-icon.png'  ?>" alt="whatsapp">
     </div>
